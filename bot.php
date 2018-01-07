3ade345b4adbf23227e6b17f523344785bed70fe
noklinebot <34377239+noklinebot@users.noreply.github.com>
2018-01-07 08:32:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,10 +14,9 @@ if (!is_null($events['events'])) {
    // Get text sent
    $text = $event['message']['text'];
    
-   
-$dbconn = pg_connect("host=ec2-54-225-255-132.compute-1.amazonaws.com port=5432 dbname=d1fcitdn1516dv user=roxhkyiabreyva password=b895f0848a866f6590be13f6b843b6bce4a9a875137c0e8f635722c2535500c5 sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
-
-pg_query_params($dbconn, 'INSERT INTO lotto(msg, sender) VALUES ($1, $2);', array($text, "sender"));
+   // Insert postgreSQL
+   $dbconn = pg_connect("host=ec2-54-225-255-132.compute-1.amazonaws.com port=5432 dbname=d1fcitdn1516dv user=roxhkyiabreyva password=b895f0848a866f6590be13f6b843b6bce4a9a875137c0e8f635722c2535500c5 sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+   pg_query_params($dbconn, 'INSERT INTO lotto(msg, sender) VALUES ($1, $2);', array($text, "sender"));
 
    // Get replyToken
    $replyToken = $event['replyToken'];
