5bc300ab0dbbf310ef44a8f506338fd0019c87dc
noklinebot <34377239+noklinebot@users.noreply.github.com>
2018-01-14 18:53:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
    
     // Insert postgreSQL
    $dbconn = pg_connect("host=ec2-54-225-255-132.compute-1.amazonaws.com port=5432 dbname=d1fcitdn1516dv user=roxhkyiabreyva password=b895f0848a866f6590be13f6b843b6bce4a9a875137c0e8f635722c2535500c5 sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
-   pg_query_params($dbconn, 'INSERT INTO lotto(msg, sender) VALUES ($1, $2);', array($text, $result));
+   pg_query_params($dbconn, 'INSERT INTO lotto(msg, sender) VALUES ($1, $2);', array($text, $replyToken));
    
 $GLOBALS["checktype"] = "";
 $GLOBALS["ArrNo"]=0;
