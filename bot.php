b4d8ff14633f45eb67ad464938622a3b564740ea
noklinebot <34377239+noklinebot@users.noreply.github.com>
2018-03-19 21:18:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,18 @@
 <?php
 $access_token = 'rufj2ZJ4iDMiyJvV9LHIom82VqtKJXW0oo5t4seLZqXCjdiXskE5HYMWWtGx09J/HfWHmb3PXO0TmkQm1XGYttMl24ckWdLZKcWx5sY4V5s1dk4W1zKuVjtM5khLwFI3uUkx5c/b2CFZ8rwk3mZyjQdB04t89/1O/w1cDnyilFU=';
 
+
+ $url = 'https://api.line.me/v1/oauth/verify';
+$headers = array('Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+//echo $result;
+
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -270,7 +282,7 @@ for($x = 0; $x < count($CorrectData); $x++) {
 
 echo "<br>";
    
-  $text = $reply . "";
+  $text = $result . "";
 for($x = 0; $x < count($ReturnData); $x++) {
 	echo $ReturnData[$x];
 	echo "<br>";
